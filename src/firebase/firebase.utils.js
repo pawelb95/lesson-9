608f8acc2fb39888f8090d99fb13048325bc5173
pawelb95 <pawebor906@student.polsl.pl>
2021-08-13 13:41:47 +0200

    implemented firebase utils, included ability to store authenticated users into our database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,15 +2,54 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+const config =  {
+  apiKey: "AIzaSyB78bclLHY3CowEPhHrrorTbayekbYJgCs",
+  authDomain: "react-test-tutor.firebaseapp.com",
+  projectId: "react-test-tutor",
+  storageBucket: "react-test-tutor.appspot.com",
+  messagingSenderId: "189659734779",
+  appId: "1:189659734779:web:3e6a28720f119a61a4065e"
 };
+
+
+// {
+//   apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+//   authDomain: 'crwn-db.firebaseapp.com',
+//   databaseURL: 'https://crwn-db.firebaseio.com',
+//   projectId: 'crwn-db',
+//   storageBucket: 'crwn-db.appspot.com',
+//   messagingSenderId: '850995411664',
+//   appId: '1:850995411664:web:7ddc01d597846f65'
+// };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.collection('users').doc(userAuth.uid);
+
+  userRef.get().then( doc => {
+    if (!doc.exists) {
+      const { displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      firestore.collection('users').doc(userAuth.uid).set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      }).then( () => {
+        console.log("user creation succesfull.");
+      }).catch(error => {
+        console.log("creation not succesfull. error: ", error);
+      })
+    } else {
+      console.log("Already exists")
+    }
+  })
+
+  return userRef;
+
+}
 
 firebase.initializeApp(config);
 

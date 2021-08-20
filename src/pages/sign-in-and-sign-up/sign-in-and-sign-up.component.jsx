c6ff0eb2dc5a2092654fa16aab9adbddab43d5c1
pawelb95 <pawebor906@student.polsl.pl>
2021-08-20 11:20:40 +0200

    Fixed the sign-in-sign-up page

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
-import { SignUpContainer } from '../../components/sign-up/sign-up.styles';
 
 import { SignInAndSignUpContainer } from './sign-in-and-sign-up.styles';
 
 const SignInAndSignUpPage = () => (
-  <SignUpContainer>
+  <SignInAndSignUpContainer>
     <SignIn />
     <SignUp />
-  </SignUpContainer>
+  </SignInAndSignUpContainer>
 );
 
 export default SignInAndSignUpPage;
